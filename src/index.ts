c2e1616326db2ba8175b572f296b72d0f26171b1
DiegoCeccon <dceccon.dextra@alelo.com.br>
2023-05-12 12:10:50 -0300

    feat: ✨ Substituição de dependência
    Substitui a regra de criptografia do InverterCripto para RealCripto, por usar a inversão de dependência somente a classe externa precisa de manutenção
    
    Como o CasoDeUso recebe uma interface "Porta" e RealCripto implementa essa "Porta", não foi necessário substituir nada da implementação do CasoDeUso

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,21 @@
-import InverterCripto from "./adapters/InverterCripto.ts";
+import ColecaoUsuarioMemoria from "./adapters/ColecaoUsuarioMemoria.ts";
 import LoginUsuario from "./core/usuario/service/LoginUsuario.ts";
 import RegistrarUsuario from "./core/usuario/service/RegistrarUsuario.ts";
+import RealCripto from "./adapters/RealCripto.ts";
 
-const provedorCripto = new InverterCripto();
+/**
+ * Instancia de implementação concreta das interfaces InverterCripto e ColecaoUsuario
+ * const provedorCripto = new InverterCripto();
+ * InverterCripto torna-se uma implementação depreciada e de forma simples esta sendo substituido
+ */
+const provedorCripto = new RealCripto();
+const colecaoUsuario = new ColecaoUsuarioMemoria();
 
-const registrar = new RegistrarUsuario(provedorCripto);
+/**
+ * Instancia do Caso de uso passando as dependências como parametro
+ * O caso de uso recebe uma interface no construtor mas aqui passamos como input a implementação concréta
+ */
+const registrar = new RegistrarUsuario(colecaoUsuario, provedorCripto);
 
 await registrar.executar({
   nome: "Diguya",
