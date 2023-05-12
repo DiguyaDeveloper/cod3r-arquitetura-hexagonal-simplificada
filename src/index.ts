fcf7141ae13e609d3284912639641c2eab6e3002
DiegoCeccon <dceccon.dextra@alelo.com.br>
2023-05-12 10:26:29 -0300

    feat: ✨ adicionado validacao do login

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
+import LoginUsuario from "./usuario/service/LoginUsuario.ts";
+import RegistrarUsuario from "./usuario/service/RegistrarUsuario.ts";
 
+const registrar = new RegistrarUsuario();
+
+registrar.executar({
+  nome: "Diguya",
+  email: "diguya@email.com",
+  senha: "123456",
+});
+
+const login = new LoginUsuario();
+const usuario = await login.executar({
+  email: "diguya@email.com",
+  senha: "123456",
+});
+
+console.log(usuario);
